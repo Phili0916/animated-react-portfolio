7ec904637c0a15a8147de9219c650e4c61896838
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-12-07 19:30:30 +0100

    adding icons

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -1,10 +1,86 @@
 import './sidebar.scss'
+import { Link, NavLink } from 'react-router-dom'
+import logoS from "../../assets/images/logo-s.png"
+import logoSubtitle from "../../assets/images/logo_sub.png"
+// import codewars from "../../assets/images/codewars_1.svg"
+import { Icon } from '@iconify/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 
 const Sidebar = () => {
     return (
-        <>
-        Hello from the sidebar
-        </>
+        <div className="nav-bar">
+            <Link className="logo" to="/">
+                <img src={logoS} alt ="side navbar logo"/>
+                <img className="sub-logo" src={logoSubtitle} alt ="side navbar sub logo"/>
+            </Link>
+            <nav>
+                <NavLink 
+                    exact="true" 
+                    activeclassname="active" 
+                    to="/"
+                >
+                    <FontAwesomeIcon icon={faHome} color="4d4d4e" />
+                </NavLink>
+                <NavLink 
+                    exact="true" 
+                    activeclassname="active" 
+                    className="about-link" 
+                    to="/about"
+                >
+                    <FontAwesomeIcon icon={faUser} color="4d4d4e" />
+                </NavLink>
+                <NavLink 
+                    exact="true" 
+                    activeclassname="active"
+                    className="contact-link" 
+                    to="/contact"
+                >
+                    <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a 
+                        target='_blank' 
+                        rel='noreferrer' 
+                        href='https://www.linkedin.com/in/pwdphildavis/'
+                    >
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        target='_blank' 
+                        rel='noreferrer' 
+                        href='https://github.com/Phili0916'
+                    >
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        target='_blank' 
+                        rel='noreferrer' 
+                        href='https://www.codewars.com/users/Phili0916'
+                    >
+                        <Icon icon="simple-icons:codewars" color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        target='_blank' 
+                        rel='noreferrer' 
+                        href='https://github.com/Phili0916'
+                    >
+                        <FontAwesomeIcon icon={faCode} color="#4d4d4e" />
+                        {/* <img src={codewars} alt="codewars icon" color="#4d4d4e" /> */}
+                    </a>
+                </li>
+            </ul>
+        </div>
     )
 }
 
